e6dfc362f4e80ef4a26fbe034082cee0355ca4b6
Juhniel <juhn.sweden@gmail.com>
2023-05-29 16:30:26 +0200

    Working on soft delete

--- src/main/java/com/juhnkim/Main.java
+++ src/main/java/com/juhnkim/Main.java
@@ -12,31 +12,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        DatabaseInitializer c = new DatabaseInitializer();
-        c.createUserTable();
-        c.createAccountTable();
-        c.createTransactionTable();
+//        DatabaseInitializer c = new DatabaseInitializer();
+//        c.createUserTable();
+//        c.createAccountTable();
+//        c.createTransactionTable();
 
 
-//        Scanner scan = new Scanner(System.in);
-//        AccountRepository accountRepository = new AccountRepository();
-//        UserRepository userRepository = new UserRepository();
-//        TransactionRepository transactionRepository = new TransactionRepository(accountRepository);
-//
-//
-//        PasswordService passwordService = new PasswordService();
-//        AccountService accountService = new AccountService(accountRepository);
-//        TransactionService transactionService = new TransactionService(transactionRepository, accountService);
-//        UserService userService = new UserService(userRepository, passwordService);
-//        LoginService loginService = new LoginService(userService);
-//
-//        TransactionMenu transactionMenu = new TransactionMenu(userRepository, accountService, transactionService, scan);
-//        AccountMenu accountMenu = new AccountMenu(accountService, scan);
-//        UserMenu userMenu = new UserMenu(userService, loginService, scan);
-//        LoggedInMenu loggedInMenu = new LoggedInMenu(transactionMenu, accountMenu, userMenu, loginService, scan);
-//        LoginMenu loginMenu = new LoginMenu(loginService, scan, loggedInMenu);
-//        MainMenu mainMenu = new MainMenu(userService, accountService, loginMenu, scan);
-//
-//        mainMenu.displayMainMenu();
+        Scanner scan = new Scanner(System.in);
+        AccountRepository accountRepository = new AccountRepository();
+        UserRepository userRepository = new UserRepository();
+        TransactionRepository transactionRepository = new TransactionRepository(accountRepository);
+
+
+        PasswordService passwordService = new PasswordService();
+        AccountService accountService = new AccountService(accountRepository);
+        TransactionService transactionService = new TransactionService(transactionRepository, accountService);
+        UserService userService = new UserService(userRepository, passwordService);
+        LoginService loginService = new LoginService(userService);
+
+        TransactionMenu transactionMenu = new TransactionMenu(userRepository, accountService, transactionService, scan);
+        AccountMenu accountMenu = new AccountMenu(accountService, scan);
+        UserMenu userMenu = new UserMenu(userService, loginService, scan);
+        LoggedInMenu loggedInMenu = new LoggedInMenu(transactionMenu, accountMenu, userMenu, loginService, scan);
+        LoginMenu loginMenu = new LoginMenu(loginService, scan, loggedInMenu);
+        MainMenu mainMenu = new MainMenu(userService, accountService, loginMenu, scan);
+
+        mainMenu.displayMainMenu();
     }
 }
